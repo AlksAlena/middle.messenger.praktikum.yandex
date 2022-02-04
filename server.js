a9b5eec53a4c30c5af895306932229a14c287312
Alena Aleksina <alena.aleksina@eltex.loc>
2022-02-05 02:11:41 +0700

    [sprint_1] изменен адрес для статики для Express

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
