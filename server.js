d61d0c65c4169a94090ac5702b023ee9d7511b2e
Alena Aleksina <alena.aleksina@eltex.loc>
2022-02-04 10:43:22 +0700

    [sprint_1] изменен путь для статических файлов с static на dist

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// app.use(express.static('./'));
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
