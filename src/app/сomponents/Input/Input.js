15cd20bf1739c965c65d2492883409bf8c1bad74
alksalena <alksalena@gmail.com>
2022-08-14 22:08:58 +0700

    [sprint_2] добавлен простой компонент Input

---
+++ src/app/сomponents/Input/Input.js
@@ -0,0 +1,15 @@
+import Handlebars from "handlebars";
+import template from "./input.tmpl";
+import Block from "../../services/Block"
+
+export default class Input extends Block {
+  constructor(props) {
+    super("div", props);
+    // к обертке добавить класс className="form-field"
+  }
+
+  render() {
+    const compiled = Handlebars.compile(template);
+    return compiled(this.props);
+  }
+}
