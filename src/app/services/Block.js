c4efc6fd0baa115bf67ef46eea0b080edeffbb49
alksalena <alksalena@gmail.com>
2022-08-14 21:19:53 +0700

    [sprint_2] добавлен Block

---
+++ src/app/services/Block.js
@@ -0,0 +1,120 @@
+import EventBus from "./EventBus";
+
+class Block {
+  static EVENTS = {
+    INIT: "init",
+    FLOW_CDM: "flow:component-did-mount",
+    FLOW_CDU: "flow:component-did-update",
+    FLOW_RENDER: "flow:render"
+  };
+
+  _element = null;
+  _meta = null;
+
+  constructor(tagName = "div", props = {}) {
+    const eventBus = new EventBus();
+    this._meta = {
+      tagName,
+      props
+    };
+
+    this.props = this._makePropsProxy(props);
+
+    this.eventBus = () => eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  _registerEvents(eventBus) {
+    eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  _createResources() {
+    const { tagName } = this._meta;
+    this._element = this._createDocumentElement(tagName);
+
+  }
+
+  init() {
+    this._createResources();
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  _componentDidMount() {
+    this.componentDidMount();
+  }
+
+  componentDidMount(oldProps) {}
+
+  dispatchComponentDidMount() {
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+  }
+
+  _componentDidUpdate(oldProps, newProps) {
+    const response = this.componentDidUpdate(oldProps, newProps);
+    if (response) {
+      this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+    }
+  }
+
+  componentDidUpdate(oldProps, newProps) {
+    return true;
+  }
+
+  setProps = nextProps => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  get element() {
+    return this._element;
+  }
+
+  _render() {
+    this._element.innerHTML = this.render();
+  }
+
+  render() {}
+
+  getContent() {
+    return this.element;
+  }
+
+  _makePropsProxy(props) {
+    const block = this;
+    return new Proxy(props, {
+      get(target, prop) {
+        const value = target[prop];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target, prop, value) {
+        const oldValue = { ...target };
+        target[prop] = value;
+        block.eventBus().emit(Block.EVENTS.FLOW_CDU, oldValue, target);
+        return true;
+      },
+      deleteProperty(target, prop) {
+        throw new Error('Нет доступа');
+      },
+    });
+  }
+
+  _createDocumentElement(tagName) {
+    return document.createElement(tagName);
+  }
+
+  show() {
+    this.getContent().style.display = 'block';
+  }
+
+  hide() {
+    this.getContent().style.display = 'none';
+  }
+}
