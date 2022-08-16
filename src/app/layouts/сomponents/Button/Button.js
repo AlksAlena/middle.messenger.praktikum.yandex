36836ab49d5c578e1ab2ff336cd9b3112bc26212
alksalena <alksalena@gmail.com>
2022-08-16 23:23:23 +0700

    [sprint_2] создан компонент Button, исходный .hbs удален

--- src/app/layouts/сomponents/Button/Button.js
+++ src/app/layouts/сomponents/Button/Button.js
@@ -1,6 +1,6 @@
 import Handlebars from 'handlebars';
 import template from './button.tmpl';
-import Block from '../../services/Block';
+import Block from '../../../services/Block';
 
 export default class Button extends Block {
   constructor(props) {
@@ -9,7 +9,6 @@ export default class Button extends Block {
   }
 
   render() {
-    const compiled = Handlebars.compile(template);
-    return compiled(this.props);
+    return Handlebars.compile(template)(this.props);
   }
 }
