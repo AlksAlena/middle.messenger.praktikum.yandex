384588db45750216d1cdf8cb37479ef67f91788a
alksalena <alksalena@gmail.com>
2022-08-16 23:23:57 +0700

    [sprint_2] создан компонент ServerErrorPage, исходный .hbs удален

---
+++ src/app/layouts/pages/ServerError/index.js
@@ -0,0 +1,17 @@
+import Handlebars from 'handlebars';
+import Block from '../../../services/Block';
+import template from './tmpl';
+import Page from "../../сomponents/IndexPage/IndexPage";
+
+export default class ServerErrorPage extends Block {
+  render() {
+    return Handlebars.compile(template)(this.props);
+  }
+}
+
+const props = {
+  title: 'Ошибка сервера | Zoo-Chat',
+  pageHeader: 'Ошибка сервера',
+  link: { text: 'На главную', url: './chats.html' }
+};
+const serverErrorPage = new Page('div', {...props});
