3e9f2f2c502ed800adf014096ced2680ebb0e57f
alksalena <alksalena@gmail.com>
2022-08-16 21:41:30 +0700

    [sprint_2] добавлены модули с реализацией fetch api

---
+++ src/app/services/Fetch.js
@@ -0,0 +1,67 @@
+const METHODS = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE'
+};
+
+function queryStringify(data) {
+  if (typeof data !== 'object') {
+    throw new Error('Data must be object');
+  }
+
+  const keys = Object.keys(data);
+  return keys.reduce((result, key, index) => {
+    return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
+  }, '?');
+}
+
+export default class HTTPTransport {
+  get = (url, options = {}) => {
+    return this.request(url, {...options, method: METHODS.GET}, options.timeout);
+  };
+
+  post = (url, options = {}) => {
+    return this.request(url, {...options, method: METHODS.POST}, options.timeout);
+  };
+
+  put = (url, options = {}) => {
+    return this.request(url, {...options, method: METHODS.PUT}, options.timeout);
+  };
+
+  delete = (url, options = {}) => {
+    return this.request(url, {...options, method: METHODS.DELETE}, options.timeout);
+  };
+
+  request = (url, options, timeout = 5000) => {
+    const { headers = {}, data, method } = options;
+
+    return new Promise((resolve, reject) => {
+      if (!method) {
+        reject('No method');
+        return;
+      }
+
+      const isGet = method === METHODS.GET;
+      const targetUrl = isGet || !!data ? `${url}${queryStringify(data)}` : url;
+
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, targetUrl);
+      Object.entries(headers).forEach(([header, value]) => xhr.setRequestHeader(header, value));
+
+      xhr.onload = () => resolve(xhr);
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+
+      xhr.timeout = timeout;
+      xhr.ontimeout = reject;
+
+      if (isGet || !data) {
+        xhr.send();
+      } else {
+        xhr.send(JSON.stringify(data));
+      }
+    });
+  };
+}
---
+++ src/app/utils/fetchWithRetry.js
@@ -0,0 +1,14 @@
+export default function fetchWithRetry(url, options = {}) {
+  const {tries = 1} = options;
+
+  function onError(err){
+    const triesLeft = tries - 1;
+    if (!triesLeft){
+      throw err;
+    }
+
+    return fetchWithRetry(url, {...options, tries: triesLeft});
+  }
+
+  return fetch(url, options).catch(onError);
+}
