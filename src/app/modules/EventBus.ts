bd61d63a6a0d34af9ff9576427d26a76650bb445
alksalena <alksalena@gmail.com>
2022-08-21 22:24:53 +0700

    [sprint_2] добавлена конфигурация для typescript и EventBus.ts

---
+++ src/app/modules/EventBus.ts
@@ -0,0 +1,30 @@
+export default class EventBus {
+  listeners: { [key: string]: Array<() => void> };
+
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event, callback): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: () => void): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event]
+      .filter(listener => listener !== callback);
+  }
+
+  // error: A spread argument must either have a tuple type or be passed to a rest parameter
+  emit(event: string, ...args: any): void {
+    // @ts-ignore
+    this.listeners[event].forEach(listener => listener(...args));
+  }
+}
