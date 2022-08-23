9d100e0efb8080e4a5cc209820dd426503f09b83
alksalena <alksalena@gmail.com>
2022-08-23 23:36:29 +0700

    [sprint_2] добавлен компонент SignUpPage, исходный .hbs удален

---
+++ src/app/pages/sign-up-page/index.ts
@@ -0,0 +1,1 @@
+export { SignUpPage as default} from './sign-up-page';
---
+++ src/app/pages/sign-up-page/sign-up-page.ts
@@ -0,0 +1,42 @@
+import { Block } from '../../modules/block';
+import template from './sign-up-page.hbs';
+import Button from '../../components/button';
+
+interface SignUpPageProps {
+  title: string;
+  fields: { [key: string]: string }[];
+  button: Block;
+}
+
+export class SignUpPage extends Block {
+  constructor() {
+    const button = new Button({
+      class: 'button',
+      text: 'Зарегистрироваться',
+      type: 'submit',
+      events: {
+        click: () => console.log('click!')
+      }
+    });
+
+    // title: Регистрация | Zoo-Chat
+    const props: SignUpPageProps = {
+      title: 'Регистрация',
+      fields: [
+        { name: 'first_name', inputClass: 'input', type: 'text', labelClass: 'label', labelText: 'Имя'},
+        { name: 'second_name', inputClass: 'input', type: 'text', labelClass: 'label', labelText: 'Фамилия'},
+        { name: 'login', inputClass: 'input', type: 'text', labelClass: 'label', labelText: 'Логин'},
+        { name: 'password', inputClass: 'input', type: 'password', labelClass: 'label', labelText: 'Пароль'},
+        { name: 'email', inputClass: 'input', type: 'email', labelClass: 'label', labelText: 'Почта'},
+        { name: 'phone', inputClass: 'input', type: 'phone', labelClass: 'label', labelText: 'Телефон'},
+      ],
+      button: button
+    };
+
+    super(props);
+  }
+
+  protected render(): DocumentFragment {
+    return this.compile(template, {...this.props});
+  }
+}
