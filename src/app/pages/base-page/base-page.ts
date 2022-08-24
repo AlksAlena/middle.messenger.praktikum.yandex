6402c5ca8f6eecf3cdcaf22c106129ac543b8e85
alksalena <alksalena@gmail.com>
2022-08-25 01:17:00 +0700

    [sprint_2] реализован роутер, сами роуты вынесены в утилиты

--- src/app/pages/base-page/base-page.ts
+++ src/app/pages/base-page/base-page.ts
@@ -2,12 +2,34 @@ import { Block } from '../../modules/block';
 import template from './base-page.hbs';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import { routes } from '../../utils/routes';
 
 export class BasePage extends Block {
+  private readonly _routes = { ...routes };
+
   constructor(props: { content: Block }) {
     const header = new Header();
     const footer = new Footer();
-    super({ ...props, header, footer });
+    const events = {
+      click: (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const target = e.target;
+        const isLink = target.hasAttribute('href');
+        const navLink = target.classList.contains('navigation');
+
+        if (isLink && navLink) {
+          const url = target.getAttribute('href');
+          this.goToPage(url);
+        }
+      }
+    };
+
+    super({ ...props, header, footer, events });
+  }
+
+  private goToPage(url: string): void {
+    this.setProps({ content: this._routes[url] });
   }
 
   protected render(): DocumentFragment {
---
+++ src/app/utils/routes.ts
@@ -0,0 +1,20 @@
+import IndexPage from '../pages/index-page';
+import ChatsPage from '../pages/chats-page';
+import ErrorPage from '../pages/error-page';
+import ProfilePage from '../pages/profile-page';
+import SignUpPage from '../pages/sign-up-page';
+
+export const routes = {
+  'index.html': new IndexPage(),
+  'chats.html': new ChatsPage(),
+  '404.html': new ErrorPage({
+    link: { text: 'На главную', url: 'index.html'},
+    message: '404. Страница не существует'
+  }),
+  '5**.html': new ErrorPage({
+    link: { text: 'На главную', url: 'index.html'},
+    message: '5**. Ошибка сервера'
+  }),
+  'profile.html': new ProfilePage(),
+  'sign-up.html': new SignUpPage()
+};
