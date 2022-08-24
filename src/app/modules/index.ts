68ac0f2a2a9c87b75e8abc790015833998fd03f6
alksalena <alksalena@gmail.com>
2022-08-24 22:54:32 +0700

    [sprint_2] удалено лишнее из index.ts

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,17 +1,9 @@
 import { render } from '../utils/render';
 import BasePage from '../pages/base-page';
 import IndexPage from '../pages/index-page';
-import SignUpPage from '../pages/sign-up-page';
 
 document.addEventListener('DOMContentLoaded', () => {
   const indexPage = new IndexPage();
-  const signUpPage = new SignUpPage();
-  const basePage = new BasePage({ content: signUpPage });
+  const basePage = new BasePage({ content: indexPage });
   render('#app', basePage);
-
-  // не сработало обновление пропса
-  // setTimeout(() => {
-  //   console.log('timeout work!');
-  //   basePage.setProps({ content: '<div>new content</div>'});
-  // }, 2000);
 });
